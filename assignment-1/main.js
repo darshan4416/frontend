fed1596a1358e52931881db6237a1f0e86d613e9
darshan4416 <darshanhirekurubar4416@gmail.com>
2022-01-05 10:14:04 -0600

    assingment-1 completed

--- assignment-1/main.js
+++ assignment-1/main.js
@@ -1,1 +1,14 @@
-alert("successfully JavaScript connected !!!")
+window.addEventListener("click",(e)=>{
+
+    const val = e.target.className;
+
+    if(val==="circle-1"){
+        alert("c-1 Violet");
+    }
+    if(val==="circle-2"){
+        alert("c-2 yellow");
+    }
+    if(val==="circle-3"){
+        alert("c-3 Red");
+    }
+});
