924f0fd1870e65ffbe452f8bb1bc82baf9203be9
darshan4416 <darshanhirekurubar4416@gmail.com>
2022-01-08 14:13:56 -0600

    currency converter completed

--- angular-day-1/src/app/app.component.ts
+++ angular-day-1/src/app/app.component.ts
@@ -7,13 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-day-1';
-  nameArray: (string)[] = [];
-  name = '';
-  idx = 0;
+  currIndex : any;
+  currValue:any;
+  tarIndex:any;
+  data: any = [
+    { name: 'India Rupee', symbol: '₹', value: { '₹': 1, '$': 0.013450394, '৳': 1.1543927 } }, 
+    { name: 'United States Dollar', symbol: '$', value: { '₹': 74.3417, '$': 1, '৳': 85.90755} },
+    { name: 'Bangladeshi taka', symbol: '৳', value: { '₹': 0.866256, '$': 0.0116404, '৳': '1'} }, 
+  ]
+  res:any = 0;
 
-  onSave(){
-    this.nameArray.push(this.name);
-    console.log(this.nameArray);
+  handler(){
+    this.res = this.data[this.currIndex].value[this.data[this.tarIndex].symbol] * this.currValue;
   }
+
+
+  
   
 }
